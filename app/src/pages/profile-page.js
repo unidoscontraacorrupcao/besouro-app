97cc66938ae9589edf9e0e7420e48696394bd84e
Jonathan Moraes <arkyebr@gmail.com>
2018-07-02 19:09:46 -0300

    Update profile css
    push-dev-image

--- app/src/pages/profile-page.js
+++ app/src/pages/profile-page.js
@@ -9,198 +9,202 @@ import '@polymer/paper-toggle-button/paper-toggle-button.js';
 import '../app-elements/app-actions.js';
 import '../app-elements/app-icons.js';
 import '../app-elements/app-dialog.js';
-import '../app-elements/shared-styles.js';
 import '../profile-elements/password-dialog.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+
 class ProfilePage extends PolymerElement {
   static get template() {
     return html`
-    <style include="shared-styles"></style>
-    <style>
-      :host {
-        display: block;
-        background: var(--secondary-background-color);
-        height: 100vh;
-        @apply --default-font;
-      }
-      app-header {
-        --app-form-header-background: white;
-        --app-form-header-color: #333333;
-      }
-      app-toolbar:not([tab-bar]) {
-        color: white;
-        padding: 0;
-        height: 250px;
-        --layout-center_-_align-items: normal;
-      }
-      app-toolbar > paper-icon-button.left {
-        margin: 20px 0 0 20px;
-      }
-      app-toolbar > h1[main-title] {
-        width: 100%;
-        display: flex
-      }
-      h1[main-title] > div.wrap-title {
-        align-self: flex-end;
-        font-family: Folio;
-        font-size: 32px;
-        line-height: 35px;
-        font-weight: normal;
-      }
-      div.wrap-title > div.status {
-        font-size: 14px;
-        line-height: 19px;
-      }
-      app-toolbar > paper-icon-button.right {
-        margin: 20px 20px 0 0;
-      }
-      app-toolbar > iron-image {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        z-index: -2;
-      }
-      .image-filter {
-        position: absolute;
-        background: rgba(49,39,131,0.5);
-        width: 100%;
-        height: 100%;
-        z-index: -1;
-      }
-      .tabs-text {
-        font-family: Folio;
-        text-transform: uppercase;
-        font-size: 18px;
-        line-height: 19px;
-      }
-      paper-input paper-button {
-        color: var(--accent-color);
-        border: none;
-      }
-      .fill {
-        flex: 1;
-      }
+      <style>
+        :host {
+          display: flex;
+          background: #f5f5f5;
+          flex-direction: column;
+          min-height: 100vh;
+        }
+        paper-toast {
+          --paper-toast-color: white;
+          --paper-toast-background-color: #e7007e;
+          left: auto !important;
+        }
+        app-header {
+          --app-form-header-background: white;
+          --app-form-header-color: #333333;
+        }
+        app-toolbar:not([tab-bar]) {
+          color: white;
+          padding: 0;
+          height: 250px;
+          --layout-center_-_align-items: normal;
+        }
+        app-toolbar > paper-icon-button.left {
+          margin: 20px 0 0 20px;
+        }
+        app-toolbar > h1[main-title] {
+          width: 100%;
+          display: flex
+        }
+        h1[main-title] > div.wrap-title {
+          align-self: flex-end;
+          font-family: Folio;
+          font-size: 32px;
+          line-height: 35px;
+          font-weight: normal;
+        }
+        div.wrap-title > div.status {
+          font-size: 14px;
+          line-height: 19px;
+        }
+        app-toolbar > paper-icon-button.right {
+          margin: 20px 20px 0 0;
+        }
+        app-toolbar > iron-image {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          z-index: -2;
+        }
+        .image-filter {
+          position: absolute;
+          background: rgba(49,39,131,0.5);
+          width: 100%;
+          height: 100%;
+          z-index: -1;
+        }
+        .tabs-text {
+          font-family: Folio;
+          text-transform: uppercase;
+          font-size: 18px;
+          line-height: 19px;
+        }
+        paper-input paper-button {
+          color: var(--accent-color);
+          border: none;
+        }
+        .fill {
+          flex: 1;
+        }
 
-      .cover paper-icon-button {
-        display: block;
-        margin: -40px auto;
-        color: white;
-        background: rgba(0,0,0,0.4);
-        width: 100%;
-      }
-      .image paper-spinner {
-        margin: -50px 60px 35px;
-      }
-      .username,
-      .notify,
-      .email {
-        max-width: 340px;
-        margin: auto;
-        padding: 0px 20px 0;
-      }
-      .email {
-        padding-top: 0;
-      }
-      .notify {
-        display: flex;
-        padding-top: 20px;
-      }
-      .notify span {
-        flex: 1;
-        font-size: 1.2rem;
-        font-weight: bold;
-      }
-      paper-button {
-        max-width: 340px;
-        margin: 20px auto 100px;
-        display: block;
-        text-align: center;
-        border-radius: 20px;
-      }
-      #upload {
-        display: block;
-        height: 0;
-        width: 0;
-      }
+        .cover paper-icon-button {
+          display: block;
+          margin: -40px auto;
+          color: white;
+          background: rgba(0,0,0,0.4);
+          width: 100%;
+        }
+        .image paper-spinner {
+          margin: -50px 60px 35px;
+        }
+        .username,
+        .notify,
+        .email {
+          max-width: 340px;
+          margin: auto;
+          padding: 0px 20px 0;
+        }
+        .email {
+          padding-top: 0;
+        }
+        .notify {
+          display: flex;
+          padding-top: 20px;
+        }
+        .notify span {
+          flex: 1;
+          font-size: 1.2rem;
+          font-weight: bold;
+        }
+        paper-button {
+          max-width: 340px;
+          margin: 20px auto 100px;
+          display: block;
+          text-align: center;
+          border-radius: 20px;
+        }
+        #upload {
+          display: block;
+          height: 0;
+          width: 0;
+        }
 
-    </style>
+      </style>
 
-    <app-dialog id="dialog">
-      <password-dialog id="passDialog" on-confirm-password="_updatePassword"></password-dialog>
-    </app-dialog>
+      <app-dialog id="dialog">
+        <password-dialog id="passDialog" on-confirm-password="_updatePassword"></password-dialog>
+      </app-dialog>
 
-    <app-actions on-go-to-inbox="_returnToInbox"></app-actions>
+      <app-actions on-go-to-inbox="_returnToInbox"></app-actions>
 
-    <app-header-layout has-scrolling-region="">
-      <app-header slot="header">
-        <app-toolbar>
-          <paper-icon-button class="left" icon="app:arrow-back" on-tap="_returnToInbox"></paper-icon-button>
-          <h1 main-title>
-            <div class="wrap-title">
-              <div class="title-name">
-                {{user.displayName}}
+      <app-header-layout has-scrolling-region="">
+        <app-header slot="header">
+          <app-toolbar>
+            <paper-icon-button class="left" icon="app:arrow-back" on-tap="_returnToInbox"></paper-icon-button>
+            <h1 main-title>
+              <div class="wrap-title">
+                <div class="title-name">
+                  {{user.displayName}}
+                </div>
+                <div class="status">
+                  Status do participante
+                </div>
               </div>
-              <div class="status">
-                Status do participante
+            </h1>
+            <paper-icon-button class="right" icon="app:edit-profile" on-tap="uploadImage"></paper-icon-button>
+            <iron-image class="image" id="profile" src="{{user.photoURL}}" sizing="cover"></iron-image>
+            <div class="image-filter"></div>
+          </app-toolbar>
+          <app-toolbar tab-bar>
+            <paper-tabs selected="{{tab}}" fallback-selection="0">
+              <paper-tab>
+                <span class="tabs-text">
+                  infos
+                </span>
+              </paper-tab>
+              <paper-tab>
+                <span class="tabs-text">
+                  troféus
+                </span>
+              </paper-tab>
+              <paper-tab>
+                <span class="tabs-text">
+                  contribuições
+                </span>
+              </paper-tab>
+            </paper-tabs>
+          </app-toolbar>
+        </app-header>
+        <iron-pages selected="{{tab}}">
+          <div id="info">
+            <input type="file" id="upload" on-change="getPhoto" accept=".jpg, .jpeg, .png">
+            <div class="fill">
+              <div class="username">
+                <paper-input label="nome" value="{{data.displayName}}" minlength="5" auto-validate="" error-message="O nome deve ter no mínimo 5 caracteres."></paper-input>
               </div>
+              <div class="email">
+                <paper-input label="email" type="email" value="{{data.email}}" disabled=""></paper-input>
+              </div>
+              <div class="email">
+                <paper-input label="senha" type="password" value="{{data.password}}" minlength="8" auto-validate="" error-message="A senha deve ter no mínimo 8 caracteres." disabled="{{!passProvider}}">
+                </paper-input>
+                <paper-input label="confirmação de senha" type="password" value="{{data.confirmpass}}" disabled="{{!passProvider}}">
+                </paper-input>
+              </div>
+              <hr>
+              <div class="notify">
+                <span>Receber notificações</span> <paper-toggle-button checked=""></paper-toggle-button>
+              </div>
+              <paper-button class="accent" on-tap="saveUserData">Salvar</paper-button>
             </div>
-          </h1>
-          <paper-icon-button class="right" icon="app:edit-profile" on-tap="uploadImage"></paper-icon-button>
-          <iron-image class="image" id="profile" src="{{user.photoURL}}" sizing="cover"></iron-image>
-          <div class="image-filter"></div>
-        </app-toolbar>
-        <app-toolbar tab-bar>
-          <paper-tabs selected="{{tab}}" fallback-selection="0">
-            <paper-tab>
-              <span class="tabs-text">
-                infos
-              </span>
-            </paper-tab>
-            <paper-tab>
-              <span class="tabs-text">
-                troféus
-              </span>
-            </paper-tab>
-            <paper-tab>
-              <span class="tabs-text">
-                contribuições
-              </span>
-            </paper-tab>
-          </paper-tabs>
-        </app-toolbar>
-      </app-header>
-      <iron-pages selected="{{tab}}">
-        <div id="info">
-          <input type="file" id="upload" on-change="getPhoto" accept=".jpg, .jpeg, .png">
-          <div class="fill">
-            <div class="username">
-              <paper-input label="nome" value="{{data.displayName}}" minlength="5" auto-validate="" error-message="O nome deve ter no mínimo 5 caracteres."></paper-input>
-            </div>
-            <div class="email">
-              <paper-input label="email" type="email" value="{{data.email}}" disabled=""></paper-input>
-            </div>
-            <div class="email">
-              <paper-input label="senha" type="password" value="{{data.password}}" minlength="8" auto-validate="" error-message="A senha deve ter no mínimo 8 caracteres." disabled="{{!passProvider}}">
-              </paper-input>
-              <paper-input label="confirmação de senha" type="password" value="{{data.confirmpass}}" disabled="{{!passProvider}}">
-              </paper-input>
-            </div>
-            <hr>
-            <div class="notify">
-              <span>Receber notificações</span> <paper-toggle-button checked=""></paper-toggle-button>
-            </div>
-            <paper-button class="accent" on-tap="saveUserData">Salvar</paper-button>
           </div>
-        </div>
-        <div id="trophies">
-          Troféus
-        </div>
-        <div id="contributions">
-          Contribuições
-        </div>
-      </iron-pages>
-    </app-header-layout>
-`;
+          <div id="trophies">
+            Troféus
+          </div>
+          <div id="contributions">
+            Contribuições
+          </div>
+        </iron-pages>
+      </app-header-layout>
+    `;
   }
 
   static get is() { return 'profile-page'; }
