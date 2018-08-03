7bef776aac7a1c1d8ecc9fee150cdf9e131b8591
David Carlos <ddavidcarlos1392@gmail.com>
2018-08-03 13:04:58 -0300

    Change env address during build process.

--- app/app_build.sh
+++ app/app_build.sh
@@ -12,12 +12,14 @@ if [[ -n $enable_analytics ]]; then
    sed -i 's#analytics-->##g' index.html
    polymer build --preset es6-bundled
    mv build build-prod
+   sed -i "s@http://localhost:8000@https://app.unidoscontraacorrupcao.org.br@g" build-prod/es6-bundled/src/app-elements/app-shell.js 
    tar -czvf build.tar.gz build-prod
    rm -rf build-prod
    git checkout src/login-elements/sign-up-view.js
    git checkout index.html
 else
     polymer build --preset es6-bundled
+    sed -i "s@http://localhost:8000@https://dev.besouro.ejplatform.org@g" build/es6-bundled/src/app-elements/app-shell.js
     tar -czvf build.tar.gz build
     rm -rf build
 fi
