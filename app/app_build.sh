e2b33b59aa3e74bb4f0d22f6243a2d4b8a996518
David Carlos <ddavidcarlos1392@gmail.com>
2018-08-30 10:44:18 -0300

    Add google api key on app build.

--- app/app_build.sh
+++ app/app_build.sh
@@ -6,22 +6,20 @@ rm -rf build
 enable_analytics=$1
 
 if [[ -n $enable_analytics ]]; then
-   sed -i 's#<!--analytics##g' src/login-elements/sign-up-view.js
-   sed -i 's#analytics-->##g' src/login-elements/sign-up-view.js
    sed -i 's#<!--analytics##g' index.html
    sed -i 's#analytics-->##g' index.html
    polymer build --preset es6-bundled
    mv build build-prod
-   sed -i "s@http://localhost:8000@https://app.unidoscontraacorrupcao.org.br@g" build-prod/es6-bundled/src/app-elements/app-shell.js 
+   sed -i "s@http://localhost:8000@https://app.unidoscontraacorrupcao.org.br@g" build-prod/es6-bundled/src/app-elements/app-shell.js
+    sed -i "s@https://maps.googleapis.com/maps/api/js?key=@https://maps.googleapis.com/maps/api/js?key=AIzaSyCy8kVxay78Uqku6yXbFTy2ZeFsHAQir54@g" build-prod/es6-bundled/index.html
    tar -czvf build.tar.gz build-prod
    rm -rf build-prod
-   git checkout src/login-elements/sign-up-view.js
    git checkout index.html
 else
     polymer build --preset es6-bundled
     sed -i "s@http://localhost:8000@https://dev.besouro.ejplatform.org@g" build/es6-bundled/src/app-elements/app-shell.js
+    sed -i "s@https://maps.googleapis.com/maps/api/js?key=@https://maps.googleapis.com/maps/api/js?key=AIzaSyCy8kVxay78Uqku6yXbFTy2ZeFsHAQir54@g" build/es6-bundled/index.html
     tar -czvf build.tar.gz build
     rm -rf build
 fi
-
 scp build.tar.gz besouro:/tmp
